0e502f2a7eeaa68726b56818df689f1b0eda7a4b
Jason Hobbs <grimmstede@feemon.com>
2014-07-16 10:32:15 -0500

    added menu highlight

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
+  before_action :require_signin
   before_action :get_user
   def index
     @group = Group.find_by(:name => 'main')
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+  before_action :require_signin
+  before_action :get_user
+  def new
+
+  end
 end
