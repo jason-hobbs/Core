3623f21551132a75de05d2d28a2269ff5ad8b499
grimmstede <grimmstede@feemon.com>
2014-07-18 22:17:14 -0500

    post create work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
   before_action :require_signin
   before_action :get_user
+  before_action :get_group
   def new
+    @post = Post.new
+  end
 
+
+  def update
+    fail
+  end
+
+  def create
+    fail
+  end
+
+  private
+
+  def get_group
+    @group = Group.find(params[:group_id])
   end
 end
