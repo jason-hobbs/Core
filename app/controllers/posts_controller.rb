d8d3f23271e410b0200de923c1d392beec83cc44
grimmstede <grimmstede@feemon.com>
2014-07-20 15:55:39 -0500

    finished new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   before_action :get_user
   before_action :get_group
   def new
+    
     @post = Post.new
   end
 
@@ -12,12 +13,22 @@ class PostsController < ApplicationController
   end
 
   def create
-    fail
+    @post = Post.new(post_params)
+    @post.group_id = params[:group_id]
+    if @post.save
+      redirect_to root_path, :gflash => { :success => "Posted successfully" }
+    else
+      render :new
+    end
   end
 
   private
 
   def get_group
     @group = Group.find(params[:group_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:entry)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :group
+  validates :entry, presence: true
+  validates :group_id, presence: true
 end
