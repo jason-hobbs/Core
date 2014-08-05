1bdf14eaba17cf4d6e88728bb47e2befc7e82002
Jason Hobbs <grimmstede@feemon.com>
2014-08-05 08:52:19 -0500

    added posts dependent destroy on groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :user
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :groupmembers, dependent: :destroy
   has_many :users, through: :groupmembers
 end
