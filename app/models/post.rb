a9afc2b667a1bbc8ff15624c9d77463b4c8bd630
Jason Hobbs <grimmstede@feemon.com>
2014-08-26 14:55:26 -0500

    added length validation for text entry

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
   has_many :replies
-  validates :entry, presence: true
   validates :title, presence: true
   validates :group_id, presence: true
   validates :tag_id, presence: true
   validates :user_id, presence: true
+  validates_length_of :entry, :in => 2..10000, :allow_blank => false
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ActiveRecord::Base
-  validates :entry, presence: true
+  validates_length_of :entry, :in => 2..10000, :allow_blank => false
   belongs_to :post
   belongs_to :user
 end
