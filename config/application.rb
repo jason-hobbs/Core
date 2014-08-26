d5e133f209b76b4b1688e116f9b350800dc6e9c9
Jason Hobbs <grimmstede@feemon.com>
2014-08-26 10:40:04 -0500

    precompile

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Core
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile += Ckeditor.assets
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)    
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += Ckeditor.assets
   config.assets.compile = true
 
   # Generate digests for assets URLs.
