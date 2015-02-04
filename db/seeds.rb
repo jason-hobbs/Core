be6d447b4ed05549096ced24ce5d8a04ad3f5722
Jason Hobbs <grimmstede@feemon.com>
2015-02-04 15:11:09 -0600

    added readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(:name=>'Jason Hobbs', :email=>'grimmstede@feemon.com', :password=>'Testing1', :admin=>true)
+User.create(:name=>'Admin', :email=>'test@test.com', :password=>'Testing1', :admin=>true)
 User.create(:name=>'Test User', :email=>'test@feemon.com', :password=>'Testing1', :admin=>false)
 Group.create(:name=>'community', :user_id=>'1')
