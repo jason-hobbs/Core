f0f5f1d0e786c7d5f443a593d6d55b8a119cfb5d
Jason Hobbs <grimmstede@feemon.com>
2015-02-17 19:09:48 -0600

    cookie security and autocomplete off

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_core_session'
+Rails.application.config.session_store :cookie_store, key: '_core_session',
+                                                      expire_after: 30.days,
+                                                      :secure => Rails.env == 'production', # Only send cookie over SSL when in production mode
+                                                      :httponly => true
