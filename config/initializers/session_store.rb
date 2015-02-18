4359955f101a0b0846e1bb0fe82d848090a8454a
Jason Hobbs <grimmstede@feemon.com>
2015-02-17 19:17:55 -0600

    removed ssl for cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 Rails.application.config.session_store :cookie_store, key: '_core_session',
                                                       expire_after: 30.days,
-                                                      :secure => Rails.env == 'production', # Only send cookie over SSL when in production mode
                                                       :httponly => true
