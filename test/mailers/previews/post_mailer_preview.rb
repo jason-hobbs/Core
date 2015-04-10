471d642f7062f495879b540f0879cede8b80e8bc
Jason Hobbs <grimmstede@feemon.com>
2015-04-10 15:22:14 -0500

    email mailer

---
+++ test/mailers/previews/post_mailer_preview.rb
@@ -0,0 +1,11 @@
+# Preview all emails at http://localhost:3000/rails/mailers/post_mailer
+class PostMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/post_mailer/new_post
+  def new_post
+    group = Group.first
+    post = group.posts.first 
+    PostMailer.new_post(group, post)
+  end
+
+end
