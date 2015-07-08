b54162c9676aa076fa9519d950fb79476df0217d
Jason Hobbs <grimmstede@feemon.com>
2015-07-08 11:27:37 -0500

    added cache for group posts

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sass-rails'
 gem 'uglifier'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails'
+gem 'dalli'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, 'localhost',
+  { :expires_in => 1.day, :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
